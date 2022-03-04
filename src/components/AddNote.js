efa59666ea5ccccd65cb8eda5802b4c0c20c4d74
Navneet Vishwakarma <navneetk7048@gmail.com>
2022-03-04 21:52:25 +0530

    Add AddNote component to add new notes

---
+++ src/components/AddNote.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const AddNote = ({ handleAddNote }) => {
+  const [noteText, setNoteText] = useState("");
+  const charLimit = 200;
+
+  const handleChange = (event) => {
+    if (charLimit - event.target.value.length >= 0)
+      setNoteText(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (noteText.trim().length > 0) handleAddNote(noteText);
+
+    setNoteText("");
+  };
+
+  return (
+    <div className="note new">
+      <textarea
+        rows="8"
+        cols="10"
+        placeholder="Type to add a note..."
+        value={noteText}
+        onChange={handleChange}
+      ></textarea>
+      <div className="note-footer">
+        <small>{charLimit - noteText.length} Remaining</small>
+        <button className="save" onClick={handleSubmit}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AddNote;
