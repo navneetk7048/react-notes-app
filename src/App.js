70e1dc2c224ebdf8940f3b9dc22b60a50ca145f7
Navneet Vishwakarma <navneetk7048@gmail.com>
2022-03-04 20:24:16 +0530

    Make notes dynamic using useState hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
 import NotesList from "./components/NotesList";
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note",
+      date: "04/03/2022",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note",
+      date: "08/03/2022",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note",
+      date: "24/03/2022",
+    },
+    {
+      id: nanoid(),
+      text: "This is my new note",
+      date: "21/03/2022",
+    },
+  ]);
+
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes={notes} />
     </div>
   );
 };
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,11 @@
 import { MdDeleteForever } from "react-icons/md";
 
-const Note = () => {
+const Note = ({ id, text, date }) => {
   return (
     <div className="note">
-      <span>Hello, this is first note</span>
+      <span>{text}</span>
       <div className="note-footer">
-        <small>04/03/2022</small>
+        <small>{date}</small>
         <MdDeleteForever className="delete-icon" size="1.3em" />
       </div>
     </div>
