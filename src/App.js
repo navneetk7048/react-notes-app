e0fc3416953f90f78a7abd1751ab999b4f45145f
Navneet Vishwakarma <navneetk7048@gmail.com>
2022-03-30 13:42:26 +0530

    Changed title

--- src/App.js
+++ src/App.js
@@ -6,28 +6,7 @@ import Search from "./components/Search";
 import Header from "./components/Header";
 
 const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      id: nanoid(),
-      text: "This is my first note",
-      date: "04/03/2022",
-    },
-    {
-      id: nanoid(),
-      text: "This is my second note",
-      date: "08/03/2022",
-    },
-    {
-      id: nanoid(),
-      text: "This is my third note",
-      date: "24/03/2022",
-    },
-    {
-      id: nanoid(),
-      text: "This is my new note",
-      date: "21/03/2022",
-    },
-  ]);
+  const [notes, setNotes] = useState([]);
 
   const [searchText, setSearchText] = useState("");
 
